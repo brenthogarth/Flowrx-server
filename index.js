1b07e9e4bb360b36fba6b6f1813cbc00627b56dd
brenthogarth <brenthogarth@gmail.com>
2026-04-13 12:56:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express');
-const twilio = require('twilio');
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
-const client = twilio(accountSid, authToken);
-
 app.get('/', (req, res) => res.send('Flow RX server is running'));
 
 app.post('/send', async (req, res) => {
+  const twilio = require('twilio');
+  const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
   const { to, message } = req.body;
   try {
     const msg = await client.messages.create({
       body: message,
-      from: twilioNumber,
+      from: process.env.TWILIO_PHONE_NUMBER,
       to: to
     });
     res.json({ success: true, sid: msg.sid });
@@ -33,5 +29,5 @@ app.post('/incoming', (req, res) => {
   res.send('<Response></Response>');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Flow RX running on port ${PORT}`));
